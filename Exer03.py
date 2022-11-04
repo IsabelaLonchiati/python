1159ce694437f533d88e5d4a666c3ad31acbbab9
IsabelaLonchiati <117416321+IsabelaLonchiati@users.noreply.github.com>
2022-11-03 22:27:09 -0300

    Add files via upload

---
+++ Exer03.py
@@ -0,0 +1,17 @@
+#trabalhando com variáveis
+#num1 = 10
+#num2 = 5
+
+num1 = input('informe o primeiro valor:')
+num2 = input('informe o segundo valor:')
+
+print(type(num1))
+
+
+
+soma = num1 + num2
+print('A soma é: ' , soma)
+
+
+Soma1 = int(num1) + int(num2)
+print('A soma correta é: {}' . format(Soma1))
